37b4599f85f3bda0753236714ff00051354537ff
RobinBigus <robin.bigus@wbscodingschool.com>
2025-07-15 14:33:14 +0200

    Added chatbot-gemini-simple-streaming

--- 06-GenAI/lectures/chatbot-gemini-simple-streaming/src/components/Form.jsx
+++ 06-GenAI/lectures/chatbot-gemini-simple-streaming/src/components/Form.jsx
@@ -1,36 +1,92 @@
-import { useState } from 'react';
-import { toast } from 'react-toastify';
-import { createChat } from '../data/gemini';
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { createChat, fetchChat } from "../data/gemini";
 
 const Form = ({ setMessages, chatId, setChatId }) => {
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState("");
   const [loading, setLoading] = useState(false);
-  const handleChange = e => setPrompt(e.target.value);
-  const handleSubmit = async e => {
+  const [isStream, setIsStream] = useState(false);
+
+  const toggleChecked = () => setIsStream((prev) => !prev);
+  const handleChange = (e) => setPrompt(e.target.value);
+
+  const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      // If the prompt value is empty, alert the user
-      if (!prompt) throw new Error('Please enter a prompt');
 
-      // Disable the submit button
+      if (!prompt) throw new Error("Please enter a prompt");
+
       setLoading(true);
+
       const userMsg = {
         _id: crypto.randomUUID(),
-        role: 'user',
-        parts: [{ text: prompt }]
+        role: "user",
+        parts: [{ text: prompt }],
       };
-      setMessages(prev => [...prev, userMsg]);
 
-      const response = await createChat({ message: prompt, chatId });
+      setMessages((prev) => [...prev, userMsg]);
+
       const asstMsg = {
         _id: crypto.randomUUID(),
-        parts: [{ text: response.aiResponse }],
-        role: 'model'
+        parts: [{ text: "" }],
+        role: "model",
       };
-      setPrompt('');
-      setMessages(prev => [...prev, asstMsg]);
-      localStorage.setItem('chatId', response.chatId);
-      setChatId(response.chatId);
+
+      if (isStream) {
+        const response = await fetchChat({
+          message: prompt,
+          chatId,
+          stream: isStream,
+        });
+        // console.log(response);
+
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder();
+
+        while (true) {
+          const { done, value } = await reader.read();
+
+          if (done) break;
+
+          const chunk = decoder.decode(value, { stream: true });
+          // console.log(chunk);
+
+          const lines = chunk.split("\n");
+          // console.log(lines);
+
+          lines.forEach((line) => {
+            if (line.startsWith("data:")) {
+              const string = line.replace("data:", "");
+              const data = JSON.parse(string);
+              console.log(data);
+
+              // if we have chatId property => update chatId state and local storage
+
+              // else if data has text property => update message state
+
+              // when updating message state, check if the message exists
+              // if the message exists => add new text
+              // if it doesnt exist => add a new message to the array
+            }
+          });
+        }
+      } else {
+        const response = await createChat({
+          message: prompt,
+          chatId,
+          stream: isStream,
+        });
+
+        asstMsg.parts[0].text = response.aiResponse;
+
+        setPrompt("");
+
+        setMessages((prev) => [...prev, asstMsg]);
+
+        localStorage.setItem("chatId", response.chatId);
+
+        setChatId(response.chatId);
+      }
     } catch (error) {
       toast.error(error.message);
     } finally {
@@ -39,17 +95,33 @@ const Form = ({ setMessages, chatId, setChatId }) => {
   };
 
   return (
-    <div className='h-1/3 w-full p-8 bg-slate-600 rounded-lg shadow-md'>
+    <div className="h-1/3 w-full p-8 bg-slate-600 rounded-lg shadow-md">
       <form onSubmit={handleSubmit}>
+        <label className="flex gap-2 items-center my-2">
+          <input
+            id="stream"
+            type="checkbox"
+            className="checkbox checkbox-primary"
+            checked={isStream}
+            onChange={toggleChecked}
+            disabled={loading}
+          />
+          <span>Stream response?</span>
+        </label>
         <textarea
           value={prompt}
           onChange={handleChange}
-          id='prompt'
-          rows='5'
-          placeholder='Ask me anything...'
-          className='block w-full px-4 py-2 border border-gray-300 rounded-md shadow-xs focus:outline-hidden focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+          id="prompt"
+          rows="5"
+          placeholder="Ask me anything..."
+          className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-xs focus:outline-hidden focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
         ></textarea>
-        <button id='submit' type='submit' className='mt-4 w-full btn btn-primary' disabled={loading}>
+        <button
+          id="submit"
+          type="submit"
+          className="mt-4 w-full btn btn-primary"
+          disabled={loading}
+        >
           Submit✨
         </button>
       </form>
--- 06-GenAI/lectures/chatbot-gemini-simple-streaming/src/data/gemini.js
+++ 06-GenAI/lectures/chatbot-gemini-simple-streaming/src/data/gemini.js
@@ -1,32 +1,38 @@
 const API_URL = import.meta.env.VITE_API_URL;
-if (!API_URL) throw new Error('API URL is required, are you missing a .env file?');
+if (!API_URL)
+  throw new Error("API URL is required, are you missing a .env file?");
 const baseURL = `${API_URL}/chat`;
 
-const createChat = async body => {
+const fetchChat = async (body) => {
   const response = await fetch(baseURL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   });
+
   if (!response.ok) {
-    // If the response is not ok, throw an error by parsing the JSON response
     const { error } = await response.json();
-    throw new Error(error || 'Something went wrong');
+    throw new Error(error || "Something went wrong");
   }
+
+  return response;
+};
+
+const createChat = async (body) => {
+  const response = await fetchChat(body);
 
   const data = await response.json();
 
   return data;
 };
 
-const getChatHistory = async chatId => {
+const getChatHistory = async (chatId) => {
   const response = await fetch(`${baseURL}/${chatId}`);
   if (!response.ok) {
-    // If the response is not ok, throw an error by parsing the JSON response
     const { error } = await response.json();
-    throw new Error(error || 'Something went wrong');
+    throw new Error(error || "Something went wrong");
   }
 
   const data = await response.json();
@@ -34,4 +40,4 @@ const getChatHistory = async chatId => {
   return data;
 };
 
-export { createChat, getChatHistory };
+export { createChat, getChatHistory, fetchChat };
